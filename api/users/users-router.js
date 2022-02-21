9409a43204d7dc6057758a4d21d45f207c65abb4
Sharon Sanchez <sharontsanchez@gmail.com>
2022-02-21 17:29:25 -0600

    work in progress

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,16 @@ const router = require('express').Router()
 const { restricted } = require ('./../auth/auth-middleware')
 const User = require('./users-model')
 
+
+router.get("/", restricted, async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
 /**
   [GET] /api/users
 
@@ -26,14 +36,6 @@ const User = require('./users-model')
   }
  */
 
-router.get('/', restricted, async (req, res, next) => { 
-  try{
-    const users = await User.find()
-    res.json(users)
-  } catch(error){
-    next(error)
-  }
-})
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router;
